5020ede6800f2cfdef997016d8f8c5e406b7bb13
tryingpfq <tryingpfq@163.com>
2018-11-27 21:12:24 +0800

    缓存

--- mmorpg-server/src/main/java/com/tryingpfq/dao/provider/CacheEntityProvider.java
+++ mmorpg-server/src/main/java/com/tryingpfq/dao/provider/CacheEntityProvider.java
@@ -1,8 +1,11 @@
 package com.tryingpfq.dao.provider;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.tryingpfq.dao.entity.IEntity;
 
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * hibernate + 缓存（caffeine）
@@ -10,5 +13,19 @@ import java.io.Serializable;
  * @date 2018/11/26 20:52
  */
 public class CacheEntityProvider<T extends IEntity<ID>,ID extends Serializable> extends HibernateEntityProvider<T,ID>{
+    private LoadingCache<ID,T> cache = Caffeine.newBuilder().maximumSize(1000)
+                                        .expireAfterWrite(10,TimeUnit.MINUTES)
+                                        .build(id -> super.get(id));
 
+    @Override
+    public T get(ID id) {
+        //从缓存中获取
+        T t = cache.get(id);
+        return t;
+    }
+
+    @Override
+    public void save(T entity) {
+
+    }
 }
