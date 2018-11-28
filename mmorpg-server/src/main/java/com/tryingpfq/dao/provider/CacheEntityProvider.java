71e21a29db01a14e549eb14691e86b596c188f78
tryingpfq <tryingpfq@163.com>
2018-11-28 17:31:21 +0800

    cache and dao

--- mmorpg-server/src/main/java/com/tryingpfq/dao/provider/CacheEntityProvider.java
+++ mmorpg-server/src/main/java/com/tryingpfq/dao/provider/CacheEntityProvider.java
@@ -3,7 +3,8 @@ package com.tryingpfq.dao.provider;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.tryingpfq.dao.entity.IEntity;
-
+import com.tryingpfq.dao.exception.GeneratorException;
+import com.tryingpfq.dao.identify.GeneratorStrategyIdentify;
 import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +27,45 @@ public class CacheEntityProvider<T extends IEntity<ID>,ID extends Serializable> 
 
     @Override
     public void save(T entity) {
+        try {
+            ID id = (ID)GeneratorStrategyIdentify.getInstance().getGeneratorStrategy(getKeyGenerator()).getGeneratorKey();
+            entity.setId(id);
+        }catch (Exception e){
+            throw  new GeneratorException("策略生成的id和实体id不一致");
+        }
+        cache.put(entity.getId(),entity);
+    }
 
+    @Override
+    public void update(T entity) {
+        cache.put(entity.getId(),entity);
+    }
+
+    @Override
+    public void saveOrUpdate(T entity) {
+        cache.put(entity.getId(),entity);
+    }
+
+    @Override
+    public T loadAndCreate(ID id, ICreator<T, ID> creator) {
+        T t = get(id);
+
+        if(t != null){
+            return t;
+        }else{
+            t = creator.create(id);
+            save(t);
+        }
+        return t;
+    }
+
+    @Override
+    public void delete(ID id) {
+        cache.invalidate(id);
+    }
+
+    @Override
+    public void delete(T entity) {
+        cache.invalidate(entity.getId());
     }
 }
